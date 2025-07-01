9ecdee84e5e305af36318e2844f275f0bc23a4aa
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2025-07-01 10:48:10 +0600

    middleware

--- src/RTK/middleware/logger.js
+++ src/RTK/middleware/logger.js
@@ -1,8 +1,9 @@
 const logger = (store) => (next) => (action) => {
   const currenState = store.getState();
-  console.log(currenState);
+  console.log("\x1b[31m%s\x1b[0m", "Current State : ", currenState);
+  console.log("\x1b[32m%s\x1b[0m", "Action : ", action);
   next(action);
-  console.log(action);
+  console.log("\x1b[33m%s\x1b[0m", "Updated State : ", store.getState());
 };
 
 export default logger;
