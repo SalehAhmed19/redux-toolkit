4fc79933e362611d372fdbc36d678a909ebe8b56
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2025-07-01 09:32:35 +0600

    basic-counter-app using rtk

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <div className="p-10">
+      <Home />
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../RTK/features/counter/counterSlice";
+
+export default function Home() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex gap-5 items-center">
+      <button
+        onClick={() => dispatch(increment())}
+        className="bg-amber-200 rounded-2xl px-5 py-2 cursor-pointer"
+      >
+        Increment
+      </button>
+      <p>{count}</p>
+      <button
+        onClick={() => dispatch(decrement())}
+        className="bg-amber-200 rounded-2xl px-5 py-2 cursor-pointer"
+      >
+        Decrement
+      </button>
+    </div>
+  );
+}
